0edb3817748127fd15c13be4961fe842ec1b5223
davidpangrib001 <davidpangrib001@gmail.com>
2022-07-01 02:39:41 +0700

    @Aguz Familia

---
+++ config.js
@@ -0,0 +1,118 @@
+let fs = require('fs')
+
+let flaaa = JSON.parse(fs.readFileSync('./api/fla.json'))
+let waifu = JSON.parse(fs.readFileSync('./api/waifu.json'))
+let data = JSON.parse(fs.readFileSync('./data.json'))
+
+
+global.owner = JSON.parse(fs.readFileSync('./settings/owner.json')) // Put your number to folder /src/owner.json
+global.mods = JSON.parse(fs.readFileSync('./settings/moderator.json')) // Want some help?
+
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  bg: 'http://bochil.ddns.net',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.me',
+  pencarikode: 'https://pencarikode.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com',
+  neoxr: 'https://neoxr-api.herokuapp.com',
+  amel: 'https://melcanz.com',
+  hardianto: 'https://hardianto.xyz',
+  lol: 'https://api.lolhuman.xyz',
+  adicug: 'https://api.adiofficial.xyz',
+  males: 'https://malest.herokuapp.com'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://neoxr-api.herokuapp.com': 'yntkts',
+  'https://api.xteam.xyz': '7a99fbd6b77336d5',
+  'https://melcanz.com': 'fpcrFHZk',
+  'https://api.lolhuman.xyz': 'Papah-Chan',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.me': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
+  'https://hardianto.xyz': 'hardianto',
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll',
+  'https://api.adiofficial.xyz': '3WQL2FCXafSH'
+}
+
+global.wait = '𝗪𝗔𝗜𝗧\n𝘔𝘰𝘩𝘰𝘯 𝘛𝘶𝘯𝘨𝘨𝘶 𝘚𝘦𝘣𝘦𝘯𝘵𝘢𝘳. . .'
+global.eror = '𝖤𝗋𝗋𝗈𝗋\n𝘐𝘯𝘵𝘦𝘳𝘯𝘢𝘭 𝘚𝘦𝘳𝘷𝘦𝘳 𝘌𝘳𝘳𝘰𝘳 :/'
+
+//========Url Template Buttons=========//
+global.dtu = 'Grup Bot'
+global.urlnya = "https://chat.whatsapp.com/GfwblXDHAqF4iA27wk8Gf4"
+
+//============= callButtons =============//
+global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
+global.phn = '0'
+
+//============= Games ================//
+global.benar = '_*Benar✅*_'
+global.salah = '_*Salah❌*_'
+global.dikit = "dikit lagi, semangat ya :')"
+
+global.multiplier = 100 // The higher, The harder levelup
+
+// Sticker WM
+global.packname = '𝖮𝗇𝖾 𝖡𝗈𝗍𝗓'
+global.author = ''
+
+//=========== Requirements ==========//  
+        global.colong1 = 'Ciss 📸'
+        global.colong2 = '𝖮𝗇𝖾 𝖡𝗈𝗍𝗓'
+global.img = pickRandom(waifu)
+global.fla = pickRandom(flaaa)
+
+global.rpg = {
+  emoticon(string) {
+    string = string.toLowerCase()
+    let emot = {
+      level: '🧬',
+      limit: '🌌',
+      healt: '❤️',
+      exp: '✉️',
+      money: '💵',
+      potion: '🥤',
+      diamond: '💎',
+      common: '📦',
+      uncommon: '🎁',
+      mythic: '🗳️',
+      legendary: '🗃️',
+      pet: '🎁',
+      sampah: '🗑',
+      armor: '🥼',
+      fishingrod: '🎣',
+      pickaxe: '⛏️',
+      sword: '⚔️',
+      kayu: '🪵',
+      batu: '🪨',
+      iron: '⛓️',
+      string: '🕸️',
+      kuda: '🐎',
+      kucing: '🐈' ,
+      anjing: '🐕',
+      makananpet: '🍖',
+      gold: '👑',
+      emerald: '💚'
+    }
+    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+    if (!results.length) return ''
+    else return emot[results[0][0]]
+  }
+}
+
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
